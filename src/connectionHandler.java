6ca2e8a438e1e17f951eb166bc7b0ceaa42b3d82
henry-iMacWindows <henry.j.kent@gmail.com>
2023-08-21 23:04:46 +0100

    UDP connection wkrks and recieves data - work out how to unpack next

--- src/connectionHandler.java
+++ src/connectionHandler.java
@@ -1,26 +1,32 @@
 import java.io.*;
 import java.net.*;
+//import com.google.gson.JsonParser;
 
 public class connectionHandler {
 
     public void createConnection(String hostname, int port) {
 
         try {
+
             InetAddress address = InetAddress.getByName(hostname);
-            DatagramSocket socket = new DatagramSocket();
+            DatagramSocket socket = new DatagramSocket(port);
 
             while (true) {
 
                 byte[] buffer = new byte[2048];
-                DatagramPacket received = new DatagramPacket(buffer, buffer.length);
+                DatagramPacket received = new DatagramPacket(buffer, buffer.length, address, port);
                 socket.receive(received);
 
                 String data = new String(buffer, 0, received.getLength());
+                //JsonParser jsonParser = new JsonParser();
+
+                //JsonArray jsonArrayOutput = (JsonArray)jsonParser.parse(data);
+                //System.out.println("Output : " + jsonArrayOutput);
 
                 System.out.println(data);
                 System.out.println();
 
-                Thread.sleep(1000);
+                Thread.sleep(1000/60);
             }
 
         } catch (SocketTimeoutException e) {
