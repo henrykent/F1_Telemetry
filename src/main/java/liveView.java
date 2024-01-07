9e6223b59f2b6c76fc345386045b7381594e2f3e
henry-iMacWindows <henry.j.kent@gmail.com>
2024-01-07 18:28:46 +0000

    with test string buffer thing + now maven proj now actually can read data properly, plus start of unpack class to get the data properly. Changes up to 05/12/2023 - basically up to buggy unpack of lap data packet fixed unpack lap ALL PACKETS WILL NOW UNPACK. made output array stream and created documentation for that stream in text file [need to do appendices] started gui created top bar with no styling

--- src/main/java/liveView.java
+++ src/main/java/liveView.java
@@ -1,8 +1,8 @@
 import javax.swing.*;
 
 public class liveView {
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
+    private JButton liveViewButton;
+    private JButton compareViewButton;
+    private JButton settingsViewButton;
+    private JButton helpViewButton;
 }
